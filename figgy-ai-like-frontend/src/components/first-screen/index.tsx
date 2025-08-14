eb48e70945152648be87e8c41137cbe272f9fe8a
Radium <2043869032@qq.com>
2025-08-15 00:34:21 +0800

    feat: add image component to first screen and update styles; remove unused SCSS file

--- figgy-ai-like-frontend/src/components/first-screen/index.tsx
+++ figgy-ai-like-frontend/src/components/first-screen/index.tsx
@@ -1,7 +1,14 @@
+import Image from '@rc-component/image';
 import React from 'react';
-import Style from './index.module.scss';
+import Style from './index.module.less';
 
 const FirstScreen: React.FC = () => {
-  return <div className={Style.container}></div>;
+  return (
+    <div className={Style.container}>
+      <div className={Style.title}>
+        <Image src="https://gw.alicdn.com/imgextra/i4/O1CN01eCjF761MxxwNOMW9W_!!6000000001502-2-tps-120-116.png_170x10000.jpg_.webp" />
+      </div>
+    </div>
+  );
 };
 export default FirstScreen;
