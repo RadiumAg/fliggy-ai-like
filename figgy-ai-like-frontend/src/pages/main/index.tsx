680fc274773b508404cbd3047e584bc413991e58
Radium <2043869032@qq.com>
2025-08-15 23:32:49 +0800

    feat: 更新 index.html 的 viewport 设置；新增 main 页面样式文件并应用于 Main 组件

--- figgy-ai-like-frontend/src/pages/main/index.tsx
+++ figgy-ai-like-frontend/src/pages/main/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Bottom from '@/components/bottom';
 import FirstScreen from '@/components/first-screen';
 import Header from '@/components/header';
+import Style from './index.module.less';
 
 const Main: React.FC = () => {
   const [isFirstRender, setFirstRender] = React.useState(true);
 
   return (
-    <div>
+    <div className={Style.container}>
       <Header />
       <>{isFirstRender ? <FirstScreen /> : null}</>
       <Bottom />
